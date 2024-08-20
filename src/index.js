d0d3bc64e71a21753f3394053779832d58937186
Buzhak <Byzhak@ya.ru>
2024-08-20 21:06:31 +0300

    create base of app

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 import './index.scss';
 
+import { Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+
 import App from './components/app';
+import ErrorBoundry from './components/error-boundry';
+import {Provider} from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import RestoService from './services/resto-service';
+import RestoServiceContext from './components/resto-service-context';
+import store from './store';
+
+const restoService = new RestoService();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <Provider store={store}>
+        <ErrorBoundry>
+            <RestoServiceContext.Provider value={restoService}>
+                <Router>
+                    <App/>
+                </Router>
+            </RestoServiceContext.Provider>
+        </ErrorBoundry>
+    </Provider>
 );
